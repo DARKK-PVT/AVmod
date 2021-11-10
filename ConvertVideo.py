6147de7dc91acb6c404b255b7f3528a5d66a6068
Himanshu Yadav <54714942+HIMANSHUYADAV100@users.noreply.github.com>
2021-11-10 17:05:24 +0530

    Update ConvertVideo.py

--- ConvertVideo.py
+++ ConvertVideo.py
@@ -1,51 +1,62 @@
+import sys
+import os
+
 import keras.backend as K
 from networks.faceswap_gan_model import FaceswapGANModel
 from converter.video_converter import VideoConverter
 from detector.face_detector import MTCNNFaceDetector
 
-K.set_learning_phase(0)
+def blankfx():
+  sys.stdout = open(os.devnull, "w")
 
-RESOLUTION = 256 # 64x64, 128x128, 256x256
-assert (RESOLUTION % 64) == 0, "RESOLUTION should be 64, 128, 256"
+def conversion_fx():
+    
+  K.set_learning_phase(0)
 
-# Architecture configuration
-arch_config = {}
-arch_config['IMAGE_SHAPE'] = (RESOLUTION, RESOLUTION, 3)
-arch_config['use_self_attn'] = True
-arch_config['norm'] = "instancenorm" 
-arch_config['model_capacity'] = "standard" 
+  RESOLUTION = 256 # 64x64, 128x128, 256x256
+  assert (RESOLUTION % 64) == 0, "RESOLUTION should be 64, 128, 256"
 
-model = FaceswapGANModel(**arch_config)
+  # Architecture configuration
+  arch_config = {}
+  arch_config['IMAGE_SHAPE'] = (RESOLUTION, RESOLUTION, 3)
+  arch_config['use_self_attn'] = True
+  arch_config['norm'] = "instancenorm" 
+  arch_config['model_capacity'] = "standard" 
 
-model.load_weights(path="./models")
-mtcnn_weights_dir = "./mtcnn_weights/"
+  model = FaceswapGANModel(**arch_config)
 
-fd = MTCNNFaceDetector(sess=K.get_session(), model_path=mtcnn_weights_dir)
-vc = VideoConverter()
+  model.load_weights(path="./models1")
+  mtcnn_weights_dir = "./mtcnn_weights/"
 
-vc.set_face_detector(fd)
-vc.set_gan_model(model)
+  fd = MTCNNFaceDetector(sess=K.get_session(), model_path=mtcnn_weights_dir)
+  vc = VideoConverter()
 
-options = {
-    # ===== Fixed =====
-    "use_smoothed_bbox": True,
-    "use_kalman_filter": True,
-    "use_auto_downscaling": False,
-    "bbox_moving_avg_coef": 0.65,
-    "min_face_area": 35 * 35,
-    "IMAGE_SHAPE": model.IMAGE_SHAPE,
-    # ===== Tunable =====
-    "kf_noise_coef": 3e-3,
-    "use_color_correction": "hist_match",
-    "detec_threshold": 0.7,
-    "roi_coverage": 0.9,
-    "enhance": 0.,
-    "output_type": 3,
-    "direction": "AtoB",
-}
+  vc.set_face_detector(fd)
+  vc.set_gan_model(model)
 
-input_fn = "1.mp4"            # update req
-output_fn = "OUTPUT_VIDEO.mp4"
-duration = None 
+  options = {
+      # ===== Fixed =====
+      "use_smoothed_bbox": True,
+      "use_kalman_filter": True,
+      "use_auto_downscaling": False,
+      "bbox_moving_avg_coef": 0.65,
+      "min_face_area": 35 * 35,
+      "IMAGE_SHAPE": model.IMAGE_SHAPE,
+      # ===== Tunable =====
+      "kf_noise_coef": 3e-3,
+      "use_color_correction": "hist_match",
+      "detec_threshold": 0.7,
+      "roi_coverage": 0.9,
+      "enhance": 0.,
+      "output_type": 3,
+      "direction": "AtoB",
+  }
 
-vc.convert(input_fn=input_fn, output_fn=output_fn, options=options, duration=duration)
+  input_fn = "testNOV12.3gpp"            # update req
+  output_fn = "testNov10.mp4"
+  duration = None 
+
+  vc.convert(input_fn=input_fn, output_fn=output_fn, options=options, duration=duration)
+
+blankfx()
+conversion_fx()
